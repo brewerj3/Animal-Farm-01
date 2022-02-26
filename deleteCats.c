85696b222abc8a35c034e189018fe2c2528fe35c
brewerj3 <joshuabrewer784@gmail.com>
2022-02-25 14:16:47 -1000

    changed struct to follow requirements

--- deleteCats.c
+++ deleteCats.c
@@ -10,6 +10,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "catDatabase.h"
 #include "deleteCats.h"
